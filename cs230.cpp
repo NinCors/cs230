f1d77b714984da46453a38c03281f3b9f3ed0ea0
NinCors <chiyuc2@gmail.com>
2019-01-25 23:51:17 -0800

    fix the bug

--- cs230.cpp
+++ cs230.cpp
@@ -18,22 +18,6 @@ connection_priority[]
 memory_module[] 1 or connected, 0 for free
 memory_wait_list[] 
 
-For each time circle:
-
-    For each processor, 
-        if the memory request is -1:
-            generate the memory request with uniform distribution
-            request_memory_list[p] = random
-            memory_wait_list[random].push[p]  add the processor number on the memory module wait
-
-    For each memory module:
-            Mark the memory module connected, 
-            access the first element in memory_wait_list[memory]
-            change the memory request = -1
-            and move to the next processor
-            Add the processor wait counter++
-
----------------------
 
 float total_average_time = 0;
 float pre_wait_time = -1;
@@ -88,11 +72,11 @@ int rand_gaussian(int memory_num){
         static std::default_random_engine rand_engine (seed);
         std::uniform_int_distribution<int> res (0, memory_num-1);
         int mean = res(rand_engine);
-        cout<<"mean"<<mean<<endl;
+        //cout<<"mean"<<mean<<endl;
         static std::default_random_engine rand_engine1 (seed+1);
         std::normal_distribution<float> distribution(mean,2);
         int r = distribution(rand_engine1);
-        cout<<"res"<<r<<endl;
+        //cout<<"res"<<r<<endl;
         if(r >=0 && r< memory_num){
             return r;
         }
@@ -121,14 +105,17 @@ float getAverageTime(int processor_num, int memory_num){
     float pre_wait_time = -1;
 
     for(int i=1; i<=time_circles; ++i){
-        float wait_time = 0;
         //cout<<"Time circle : " << i<<endl;
+        for(int m = 0; m<memory_num; ++m){
+            memory_module[m] =0;
+        }   
+
 
         for(int p = 0; p<processor_num; ++p){
             //Loop all the processor, if the memory request is empty, then random generate one
             //And, push the processor with new memory request to the request_queue.
             if(memory_request[p] == -1){
-                int memory_index = rand_uniform(memory_num);
+                int memory_index = rand_gaussian(memory_num);
                 //cout<<"Memory index is "<<memory_index<<endl;;
                 memory_request[p] = memory_index;
                 request_queue.push_back(p);
@@ -138,39 +125,49 @@ float getAverageTime(int processor_num, int memory_num){
 
         int current = 1;
         while(current < request_queue.size()){
+
+            /*
+            cout<<"--------------------------"<<endl;
+            cout<<"request queue "<<endl;
+            for(int c =0; c<request_queue.size();c++){
+                cout<<request_queue[c]<<" : ";
+            }
+            cout<<endl;
+            cout<<"processor request "<<endl;
+            for(int p = 0; p<processor_num;p++){
+                cout<<memory_request[p]<<" - ";
+            }
+            cout<<endl;
+
+            cout<<"Memory module"<<endl;
+            for(int g = 0; g<memory_num;++g){
+                cout<<memory_module[g] <<" - ";
+            }
+            cout <<endl;
+            */
+        
+            
             int p_index = request_queue[current];
             if(memory_module[memory_request[p_index]] == 0){ //if the memory module is free  
                 memory_module[memory_request[p_index]] = 1; //mark the memory module as connected
                 memory_request[p_index] = -1;//mark the professor as free
-                if(request_queue.size()<=1){
-			cout<<"no queue"<<endl;		
-		}
-		request_queue.erase(request_queue.begin());//remove the first request in the request queue
+		        request_queue.erase(request_queue.begin()+current);//remove the first request in the request queue
             }
-            else{ //if the memory module is connected
-                wait_time++;
+            else if(memory_module[memory_request[p_index]] == 1){ //if the memory module is connected
+                total_wait_time++;
+                //cout<<"wait index"<<p_index<<endl;
                 current++;// The current processor need to wait for the next time circle, skip it.
             }
+            else{
+                cout<<"wrong!"<<endl;
+                return -1;
+            }
         }
-        total_wait_time += wait_time;
-        
-        
-        if( (abs(pre_wait_time - wait_time)/abs(pre_wait_time)) < 0.0002){
-            float res = total_wait_time/total_request;
-            /*
-            cout<<"Similar return"<<endl;
-            cout<<"pre "<<pre_wait_time<<endl;
-            cout<<"wait_time"<<wait_time<<endl;
-            cout<<"Total_wait_time"<<total_wait_time<<endl;
-            cout<<"res"<<res<<endl;
-            */
-            return res;    
-        }
-
-        pre_wait_time = wait_time;
     }
+    cout<<"total wait time" << total_wait_time<<endl;
+    cout<<"Total request"<< total_request<<endl;
 
-    return total_wait_time/total_request;  
+    return total_wait_time/(total_request);  
 }
 
 
@@ -181,10 +178,15 @@ int main()
     for(int p = 0; p < 6; ++p){
         ofstream myfile;
         myfile.open("Datafor"+to_string(processors_num[p]));
+        float pre_wait_time = -1;
         for(int m = 1; m <= 2048; ++m){
             float average_wait_time = getAverageTime(processors_num[p],m);
             cout<<"The average time is "<< average_wait_time << " for processor " << processors_num[p] <<" with memory "<<m<<endl;
             myfile<<average_wait_time<<endl;
+            
+            if((abs(pre_wait_time - average_wait_time)/abs(pre_wait_time)) < 0.0002){
+                break;
+            }
         }
         myfile.close();
     }
--- plot.py
+++ plot.py
@@ -6,7 +6,6 @@ processors = [2,4,8,16,32,64]
 x = []
 for i in range(1,2049):
     x.append(i)
-print x
 
 for p in processors:
     fileName = "Datafor"+str(p)
@@ -20,8 +19,9 @@ for p in processors:
         time.append(float(t)) 
     
     plt.plot(x,time)
+    plt.yscale('linear')
     plt.xlabel("Memory")
     plt.ylabel("Average wait time")
-
-    plt.title("Graph for processor " + str(p))
+    plt.title("gaussian random Graph for processor " + str(p))
+    plt.savefig("gaussian random Graph for processor" + str(p))
     plt.show()
